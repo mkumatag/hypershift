1c1b8663f123e4b5facbdb51bffc21127fbd9f96
Dharaneeshwaran Ravichandran <dharanish185@gmail.com>
2022-03-01 14:54:05 +0530

    Hypershift create infra powervs

--- cmd/infra/destroy.go
+++ cmd/infra/destroy.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/openshift/hypershift/cmd/infra/aws"
 	"github.com/openshift/hypershift/cmd/infra/azure"
+	"github.com/openshift/hypershift/cmd/infra/ibmcloud_powervs"
 )
 
 func NewDestroyCommand() *cobra.Command {
@@ -16,6 +17,7 @@ func NewDestroyCommand() *cobra.Command {
 
 	cmd.AddCommand(aws.NewDestroyCommand())
 	cmd.AddCommand(azure.NewDestroyCommand())
+	cmd.AddCommand(ibmcloud_powervs.NewDestroyCommand())
 
 	return cmd
 }
